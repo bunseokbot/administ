58e588d0550b6790737e9b659635be49370435c3
Kim Namjun <bunseokbot@uproot.im>
2017-10-19 15:29:11 +0900

    Add find_package function

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='django-administ',
     version='1.0',
+    packages=find_packages(),
     license='Apache License',
     description='A simple way to protect your error trace message',
     author='bunseokbot',
