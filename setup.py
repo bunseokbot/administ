f850c3e47eab93951dacf4f2b556c311d6305f7f
Kim Namjun <bunseokbot@uproot.im>
2017-10-24 10:17:27 +0900

    Update setup.py file

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-administ',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     license='Apache License',
     description='A simple way to protect your error trace message',
     author='bunseokbot',
     author_email='bunseokbot@gmail.com',
     url='https://github.com/bunseokbot/administ',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    python_requires='>=3',
 )
